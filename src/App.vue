0053ca5ea1340f9c60790603d66c6f2f4c22e42f
soricinae <soricinae@gmail.com>
2018-08-29 22:37:41 -0400

    Move app.vue to component

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<template>
+    <div>
+        Name: <input v-model="name" type="text">
+        <div class="alert alert-primary">Hello Component</div>
+        <hello-component :name="name" :initialEnthusiasm="5" />
+        <h1>Hello Decorator Component</h1>
+        <hello-decorator-component :name="name" :initialEnthusiasm="5" />
+    </div>
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+import HelloComponent from "./components/Hello.vue";
+import HelloDecoratorComponent from "./components/HelloDecorator.vue";
+
+export default Vue.extend({
+    data() { return { name: "App.vue" }},
+    components: {
+        HelloComponent,
+        HelloDecoratorComponent
+    }
+});
+</script>
